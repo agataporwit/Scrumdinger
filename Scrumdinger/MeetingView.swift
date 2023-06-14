4a8ca058e8631f8a9bd45ed11c4525a2e3ffc236
Agata P <43980942+agataporwit@users.noreply.github.com>
2023-06-14 11:59:22 -0700

    Update MeetingView.swift

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -1,27 +1,44 @@
-//
-//  ContentView.swift
-//  Scrumdinger
-//
-//  Created by Agata Porwit on 6/8/23.
-//
-
 import SwiftUI
-import CoreData
+
 
 struct MeetingView: View {
-    var body: some View{
+    var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello")
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
         }
         .padding()
     }
 }
 
+
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingView ()
+        MeetingView()
     }
 }
